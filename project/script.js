225f873f55c2885741bb6d23b5388e814b730c61
master <jeff73@mail.ru>
2020-05-08 19:27:54 +0300

    Это мой первый комит

---
+++ project/script.js
@@ -0,0 +1,55 @@
+var first = 1;
+let second = 2;
+const pi = 3.14;
+
+var naumber = 5;
+var string = "dorova";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('qweqwe' * 9);
+
+
+
+var und;
+console.log(und);
+
+obj = {
+	name: 'John',
+	age: 25,
+	isMarried: false
+}
+
+console.log(obj['name']);
+
+var arr = ['qwe', 'wer', 'ert'];
+console.log(arr[2]);
+
+//alert("qwe");
+//confirm("156556?");
+//var answer = +prompt("18 есть?", "18");
+
+//console.log(typeof(answer));
+
+var incr = 10,
+	decr = 10;
+
+//incr++;
+//decr--;
+
+//console.log(incr++);
+//console.log(decr--);
+
+console.log(5%2);
+	
+console.log(incr == 10);
+console.log("10" === 10);
+
+var isChecked = true;
+var isClose = true;
+
+console.log(isChecked || !isClose);
